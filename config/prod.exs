03acbbdecf06a16574cc90e1fa64d2c6ea569975
Ben H <bhudson@findmypast.com>
2017-04-07 17:12:54 +0100

    hard set secret key for now

--- config/prod.exs
+++ config/prod.exs
@@ -19,6 +19,11 @@ config :agilebible, Agilebible.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :agilebible, Agilebible.Endpoint,
+  secret_key_base: "THISWILLBEMOVEDINTOANENVVARANDCHANGED"
+
+
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
