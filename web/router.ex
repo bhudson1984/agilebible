7dc444781f23e233d4a204efae031f12244d2644
Ben H <bhudson@findmypast.com>
2017-04-08 21:11:30 +0100

    Added SASS - added dummy page

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule Agilebible.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/content/:id", ContentController, :index
   end
 
   # Other scopes may use custom stacks.
